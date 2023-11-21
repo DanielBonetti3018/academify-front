9c15aa7fe2fec34d4e45cc289e5a8e671b98da00
danielbonetti3018 <daniel.almeida6@granbery.metodista.br>
2023-11-20 21:15:56 -0300

    estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopoComponent } from './topo/topo.component';
+import { AlunosComponent } from './alunos/alunos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    TopoComponent,
+    AlunosComponent
   ],
   imports: [
     BrowserModule,
