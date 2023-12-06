d1a7f46d88f9babeb1a951f28bf86b428c09c82b
Daniel Cassio Bonetti de Almeida <daniel.almeida6@granbery.edu.dti>
2023-12-06 14:33:48 -0300

    models e home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { EditarAlunoComponent } from './editar-aluno/editar-aluno.component';
+import { CadastroAlunosComponent } from './cadastro-alunos/cadastro-alunos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  {path:'home', component:HomeComponent},
+  {path:'listar', component:AlunosComponent},
+  {path:'editar', component:EditarAlunoComponent},
+  {path:'cadastrar', component:CadastroAlunosComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/model/IListAlunos.ts
@@ -0,0 +1,7 @@
+export interface IListAlunos {
+    id: number;
+    nome: string;
+    matricula: string;
+    nascimento: Date;
+    dataHoraCadastro: Date;
+}
